78f487d1d169d1287d4ef8b954a87606d6d54dd6
Isen-kun <rajorocco@gmail.com>
2024-04-13 01:19:40 +0530

    Project form addition

--- src/ProjectForm.jsx
+++ src/ProjectForm.jsx
@@ -9,37 +9,160 @@ const ProjectForm = () => {
     };
 
     return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-            <label>Name:</label>
-            <input
-                type="text"
-                {...register('name', { required: true, maxLength: 20 })}
-            />
-            {errors.name?.type === 'required' && (
-                <span>This field is required</span>
-            )}
-            {errors.name?.type === 'maxLength' && (
-                <span>Name cannot exceed 20 characters</span>
-            )}
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label>Project Code:</label>
+                <input
+                    type="text"
+                    {...register('projectCode', { required: true, maxLength: 20 })}
+                />
+                {errors.projectCode?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.projectCode?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
 
-        <div>
-            <label>Email:</label>
-            <input
-                type="email"
-                {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
-            />
-            {errors.email?.type === 'required' && (
-                <span>This field is required</span>
-            )}
-            {errors.email?.type === 'pattern' && (
-                <span>Please enter a valid email address</span>
-            )}
-        </div>
+            <div>
+                <label>Project Name:</label>
+                <input
+                    type="text"
+                    {...register('projectName', { required: true, maxLength: 20 })}
+                />
+                {errors.projectName?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.projectName?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
 
-        <button type="submit">Submit</button>
-    </form>
+            <div>
+                <label>Project Manager:</label>
+                <input
+                    type="text"
+                    {...register('projectManager', { required: true, maxLength: 20 })}
+                />
+                {errors.projectManager?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.projectManager?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
+
+            <div>
+                <label>Address:</label>
+                <input
+                    type="text"
+                    {...register('address', { required: true, maxLength: 20 })}
+                />
+                {errors.address?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.address?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
+
+            <div>
+                <label>Project Duration:</label>
+                <input
+                    type="text"
+                    {...register('projectDuration', { required: true, maxLength: 20 })}
+                />
+                {errors.projectDuration?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.projectDuration?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
+
+            <div>
+                <label>GST Number:</label>
+                <input
+                    type="text"
+                    {...register('gstNumber', { required: true, maxLength: 20 })}
+                />
+                {errors.gstNumber?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.gstNumber?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
+
+            <div>
+                <label>Agreement Number:</label>
+                <input
+                    type="number"
+                    {...register('agreementNumber', { required: true, pattern: /^[0-9]+$/ })}
+                />
+                {errors.agreementNumber?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.agreementNumber?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
+
+            <div>
+                <label>Date:</label>
+                <input
+                    type="date"
+                    {...register('date', { required: true })}
+                />
+                {errors.date?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+            </div>
+
+            <div>
+                <label>Extended Duration:</label>
+                <input
+                    type="text"
+                    {...register('extendedDuration', { required: true, maxLength: 20 })}
+                />
+                {errors.extendedDuration?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.extendedDuration?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
+
+            <div>
+                <label>Billing Address:</label>
+                <input
+                    type="text"
+                    {...register('billingAddress', { required: true, maxLength: 20 })}
+                />
+                {errors.billingAddress?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.billingAddress?.type === 'maxLength' && (
+                    <span>Name cannot exceed 20 characters</span>
+                )}
+            </div>
+
+            <div>
+                <label>Remarks:</label>
+                <input
+                    type="text"
+                    {...register('remarks', { required: true, maxLength: 50 })}
+                />
+                {errors.remarks?.type === 'required' && (
+                    <span>This field is required</span>
+                )}
+                {errors.remarks?.type === 'maxLength' && (
+                    <span>Name cannot exceed 50 characters</span>
+                )}
+            </div>
+
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 
